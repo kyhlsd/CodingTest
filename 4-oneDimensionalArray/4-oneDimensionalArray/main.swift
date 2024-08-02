483a1a7ff4bcce42e02b9840ca47ec0f26a14b99
kyhlsd <kmyghn@gmail.com>
2024-08-02 15:54:43 +0900

    4. 일차원 배열- 공 넣기

---
+++ 4-oneDimensionalArray/4-oneDimensionalArray/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  4-oneDimensionalArray
+//
+//  Created by 김영훈 on 8/2/24.
+//
+
+import Foundation
+
+var answer = ""
+var array = [Int]()
+let inputNM = readLine()!.split(separator: " ").map { Int($0) }
+
+for _ in 0...inputNM[0]! {
+    array.append(0)
+}
+
+for _ in 0..<inputNM[1]! {
+    let inputBall = readLine()!.split(separator: " ").map { Int($0) }
+    for i in inputBall[0]!...inputBall[1]! {
+        array[i] = inputBall[2]!
+    }
+}
+
+array.removeFirst()
+for i in array {
+    answer += "\(i) "
+}
+answer.removeLast()
+print(answer)
